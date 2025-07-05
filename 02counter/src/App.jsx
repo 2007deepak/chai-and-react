325ee446a9f176ffa9b596c7a83bcf4be69b2f10
Deepak Kumar <dk4503835@gmail.com>
2025-07-05 16:42:56 +0530

    feat: added Maximum and Minimum value reach

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,30 +4,39 @@ import './App.css'
 
 function App() {
   const [counter, setCounter] = useState(15);
-  
+  const [message, setMessage] = useState(" ");
 
 
   const addValue = () => {
     console.log("clicked",counter);
-    setCounter(counter + 1);
-    
+    if(counter < 20){
+      setCounter(counter + 1);
+      setMessage("");
+     
+    }else{
+      setMessage("Maximum value reached");
+    }
 
   }
 
  const removeValue = () => {
   console.log("clicked",counter);
-  setCounter(counter - 1);
+  if(counter > 0 ) {
+    setCounter(counter - 1);
+  }else{
+    setMessage("Minimum value reached");
+  }
   
  }
   return (
     <>
       <h1>Chai aur React</h1>
-      <h2>Counter Value : </h2>
+      <h2>Counter Value :{counter} </h2>
       <button onClick={addValue}>Add value</button>
       <br />
       <br />
       <button onClick={removeValue}>Remove value</button>
-      <p style={{ color: "red", fonWeight: "bold" }}></p>
+      <p style={{ color: "red", fonWeight: "bold" }}>{message}</p>
     </>
   );
 }
