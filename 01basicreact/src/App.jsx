b1976e2d0edf67bd4fd8246d727bca9ea075a7a6
Deepak Kumar <dk4503835@gmail.com>
2025-07-05 16:04:51 +0530

    some extra feature in App.jsx

--- 01basicreact/src/App.jsx
+++ 01basicreact/src/App.jsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import './App.css'
+import Chai from "./Chai.jsx"
 function App() {
   
 
@@ -10,7 +8,7 @@ function App() {
     <>
       <h1>Haan ji kaise ho aap sabhi swagat hai mere ek aur serise chai aur react me by Hitesh Chaudhary sir </h1>
       <h2>Main aap sabhi ka pyara dost || Deepak Kumar</h2>
-       
+       <Chai/>
     </>
   )
 }
