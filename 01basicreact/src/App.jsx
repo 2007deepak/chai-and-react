0e214446f61a767bf84277e209f5684ee889ed2f
Deepak Kumar <dk4503835@gmail.com>
2025-07-05 15:55:28 +0530

    some heading added

--- 01basicreact/src/App.jsx
+++ 01basicreact/src/App.jsx
@@ -8,7 +8,8 @@ function App() {
 
   return (
     <>
-      <h1>Hello ji</h1>
+      <h1>Haan ji kaise ho aap sabhi swagat hai mere ek aur serise chai aur react me by Hitesh Chaudhary sir </h1>
+      <h2>Main aap sabhi ka pyara dost || Deepak Kumar</h2>
        
     </>
   )
