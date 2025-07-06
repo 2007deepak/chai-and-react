d91176570c7f0e8bd6c4e97eda7b18d53ef76568
Deepak Kumar <dk4503835@gmail.com>
2025-07-06 11:54:44 +0530

    feat: Added new Componet Card.jsx

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,13 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from "./components/Card.jsx"
 
 function App() {
 
+  let myobj = {
+   username: "Deepak",
+    age:21
+  }
+   let newArr = [1,2,3]
 
   return (
     <>
-      <h1 className='bg-green-400'>Hello Tailwind!</h1>
+      <h1 className="bg-green-400 text-orange-500 p-4 rounded-xl">
+        Hello Tailwind!
+      </h1>
+      <Card channel="chaiaurcode" someObj={myobj} />
+      <Card someObj={newArr}/>
+
     </>
   );
 }
