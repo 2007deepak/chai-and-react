c2873eb8fd18379bde011771012ee433b02f391b
Deepak Kumar <dk4503835@gmail.com>
2025-07-13 13:54:21 +0530

    feat: New feature added

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -19,10 +19,18 @@ useEffect (() => {
 },[])
 
   return (
-    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
-       Gtthub followers: {data.followers} 
-       <img src={data.avatar_url}  alt="Git Picture "width={300} />   </div>
-  )
+    <div className="text-center m-4 bg-gray-600  text-white p-4 text-3xl">
+      Github followers: {data.followers}
+      <div className="flex justify-center mt-4 ">
+        <img
+          className="rounded-full  cursor-pointer"
+          src={data.avatar_url}
+          alt="Git Picture"
+          width={300}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Github
